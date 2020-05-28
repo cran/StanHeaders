3969c1c0e9031118f79a30564fec405e0acfecff
Ben Goodrich <benjamin.goodrich@columbia.edu>
2020-05-28 14:30:09 +0000

    version 2.21.0-3

---
+++ R/Flags.R
@@ -0,0 +1,14 @@
+CxxFlags <- function(as_character = FALSE) {
+  CXXFLAGS <- "-D_REENTRANT -DSTAN_THREADS"
+  if (isTRUE(as_character)) return(CXXFLAGS)
+  cat(CXXFLAGS, " ")
+  return(invisible(NULL))
+}
+
+LdFlags <- function(as_character = FALSE) {
+  TBB <- system.file("lib", .Platform$r_arch, package = "RcppParallel", mustWork = TRUE)
+  PKG_LIBS <- paste0("-L", shQuote(TBB), " -Wl,-rpath ", TBB, " -ltbb -ltbbmalloc")
+  if (isTRUE(as_character)) return(PKG_LIBS)
+  cat(PKG_LIBS, " ")
+  return(invisible(NULL))
+}
